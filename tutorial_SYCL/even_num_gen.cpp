7a13013d35fd1456ef940bb07b896db86570414b
Sourabh Kulkarni <53595419+sourabh061295@users.noreply.github.com>
2023-02-12 16:33:39 +0100

    Syntax change

--- tutorial_SYCL/even_num_gen.cpp
+++ tutorial_SYCL/even_num_gen.cpp
@@ -16,8 +16,8 @@ int main(int, char **)
               << queue.get_device().get_info<cl::sycl::info::device::name>()
               << "\n";
     {
-        cl::sycl::buffer<int, 1> a_sycl(&a, cl::sycl::range<1>{LENGTH});
-        cl::sycl::buffer<int, 1> k_sycl(&k, cl::sycl::range<1>(1));
+        auto a_sycl = cl::sycl::buffer<int, 1>(&a, cl::sycl::range<1>{LENGTH});
+        auto k_sycl = cl::sycl::buffer<int, 1>(&k, cl::sycl::range<1>(1));
 
         queue.submit([&](cl::sycl::handler &cgh)
                      {
