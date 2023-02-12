c23c5164f3152abe09cdd3de4c02bde7cbb807bd
Sourabh Kulkarni <53595419+sourabh061295@users.noreply.github.com>
2023-02-12 16:18:29 +0100

    Error correction

--- tutorial_SYCL/even_num_gen.cpp
+++ tutorial_SYCL/even_num_gen.cpp
@@ -6,8 +6,8 @@ class evenNumberGenerator;
 
 int main(int, char **)
 {
-    cl::sycl::double a[LENGTH] = {0};
-    cl::sycl::int k = 2;
+    double a[LENGTH] = {0};
+    int k = 2;
 
     cl::sycl::default_selector device_selector;
 
@@ -16,20 +16,20 @@ int main(int, char **)
               << queue.get_device().get_info<cl::sycl::info::device::name>()
               << "\n";
     {
-        cl::sycl::buffer<cl::sycl::double, 1> a_sycl(&a, cl::sycl::range<1>(1));
-        cl::sycl::buffer<cl::sycl::int, 1> k_sycl(&b, cl::sycl::range<1>(1));
+        cl::sycl::buffer<double, 1> a_sycl(&a, cl::sycl::range<1>(1));
+        cl::sycl::buffer<int, 1> k_sycl(&k, cl::sycl::range<1>(1));
 
         queue.submit([&](cl::sycl::handler &cgh)
                      {
             auto a_acc = a_sycl.get_access<cl::sycl::access::mode::read_write>(cgh);
             auto k_acc = k_sycl.get_access<cl::sycl::access::mode::read>(cgh);
 
-            cgh.parallel_for<class evenNumberGenerator>(cl::sycl::range<1>{LENGTH}[=] (cl::sycl::id<1> id) {
+            cgh.parallel_for<class evenNumberGenerator>(cl::sycl::range<1>{LENGTH}, [=](cl::sycl::id<1> id) {
                 a_acc[id] = a_acc[id - 1] + k_acc[0];
             }); });
     }
 
-    std::cout << "A array:" << endl;
+    std::cout << "A array:" << std::endl;
 
     for (int i = 0; i < LENGTH; i++)
     {
